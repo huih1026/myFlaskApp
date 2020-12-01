e82352344283e2610c4ef2404462f8535ed0a59c
huih1026 <huih1026@gmail.com>
2020-12-01 00:03:45 -0800

    after output helloworld

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import sys
 from flask import Flask
-
-
+# create an app by flask
 app = Flask(__name__)
-@app
+# given the route to let the broswer to find
+@app.route("/")
+
+def index():
+	return "hello,world"
