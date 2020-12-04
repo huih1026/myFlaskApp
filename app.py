0105e34f339d5583f84deff529c8a4a2aa5ac7e3
huih1026 <huih1026@gmail.com>
2020-12-03 23:25:55 -0800

    after jumbotron and the navbar implementation

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 # given the route to let the broswer to find
 @app.route("/")
-
 def index():
 	return render_template('home.html')
+
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
 
 if __name__ == '__main__':
