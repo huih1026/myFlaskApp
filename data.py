04b87e8e379e8ca714c651f716e1277442de74dd
huih1026 <huih1026@gmail.com>
2020-12-05 23:35:13 -0800

    after implement the navbar, before the database

--- data.py
+++ data.py
@@ -1,24 +1,24 @@
-def articles():
+def Articles():
 	articles = [
 		{
-			'id': 3,
-			'name': 'article one',
+			'id': 1,
+			'title': 'article one',
 			'body': "this is article one",
-			'author': "kyle he"
+			'author': "kyle he",
+			'date': '07-20-2015'
+		},
+		{
+			'id': 2,
+			'title': 'article one',
+			'body': "this is article one",
+			'author': "kevin he",
 			'date': '07-20-2015'
 		},
 		{
 			'id': 3,
-			'name': 'article one',
+			'title': 'article one',
 			'body': "this is article one",
-			'author': "kevin he"
-			'date': '07-20-2015'
-		},
-		{
-			'id': 3,
-			'name': 'article one',
-			'body': "this is article one",
-			'author': "sophia he"
+			'author': "sophia he",
 			'date': '07-20-2015'
 		}
 	]
