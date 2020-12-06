7ae7ced38f3d4fb90c53c403d8acfb98a543f5b7
huih1026 <huih1026@gmail.com>
2020-12-05 16:47:12 -0800

    change the computer

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# myFlaskApp
+1. Track stock performance
+2. Establish a watching stock pool, monitor all highs and lows, give alert when
+   watching stock reach buypoint (setup a buypoint such as lows a precentage)
+
+3. Categorized all stocks: growth/value/swing value/ to monitor its lows to 
+   get buypoint
+
---
+++ data.py
@@ -0,0 +1,25 @@
+def articles():
+	articles = [
+		{
+			'id': 3,
+			'name': 'article one',
+			'body': "this is article one",
+			'author': "kyle he"
+			'date': '07-20-2015'
+		},
+		{
+			'id': 3,
+			'name': 'article one',
+			'body': "this is article one",
+			'author': "kevin he"
+			'date': '07-20-2015'
+		},
+		{
+			'id': 3,
+			'name': 'article one',
+			'body': "this is article one",
+			'author': "sophia he"
+			'date': '07-20-2015'
+		}
+	]
+	return articles;
